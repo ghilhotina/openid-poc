a84720532012b8eab8e0636323c42101a3ac18f9
Thiago Ghilardi <thiago.ghilardi@MacBook-Pro-de-administrator.local>
2019-07-29 18:13:18 -0300

    first commit

---
+++ openid.py
@@ -0,0 +1,58 @@
+import flask, base64, requests, jwt
+
+from flask import request
+from flask_restful import Api, Resource, reqparse
+
+app = flask.Flask(__name__)
+
+endpoint_base="https://sso-dev.apps.banestes.b.br/auth"
+endpoint_authorization=endpoint_base+"/realms/sfb-dev/protocol/openid-connect/auth"
+endpoint_token=endpoint_base+"/realms/sfb-dev/protocol/openid-connect/token"
+
+client_id="zenvia-gerenciador-web"
+client_secret="7835cd41-71fb-4f2c-832c-cc71d2ca6adc"
+redirect_uri="https://openid-return.free.beeceptor.com"
+
+@app.route("/auth")
+def auth():
+    print("Redirecting...")
+    url_redirect=(endpoint_authorization +
+                "?response_type=code" +
+                "&scope=openid" +
+                "&client_id=zenvia-gerenciador-web" +
+                "&state=af0ifjsldkj" +
+                "&redirect_uri=teste")
+    return flask.redirect(url_redirect)
+
+@app.route("/return")
+def ret():
+    state = request.args.get('state')
+    session_state = request.args.get('session_state')
+    code = request.args.get('code')
+
+    auth=requests.auth.HTTPBasicAuth(client_id, client_secret)
+
+    data = {
+        'grant_type': 'authorization_code',
+        'code': code,
+        'redirect_uri' : redirect_uri
+    }
+
+    resp = requests.post(endpoint_token, data, auth=auth, verify=False)
+    print("Resposta recebida: {} - {}".format(resp, resp.text))
+
+    if resp.status_code == 200:
+        j = resp.json()
+        print("json: {}".format(j))
+        print("id_token coded: {}".format(j['id_token']))
+        id_token_headers = jwt.get_unverified_header(j['id_token'])
+        print("unverified_headers: {}".format(id_token_headers))
+        #id_token = jwt.decode(j['id_token'], client_secret, algorithms=['RS256'])
+        id_token = jwt.decode(j['id_token'], verify=False)
+        print("id_token decoded: {}".format(id_token))
+        result_string = id_token_headers + "<br/>" + id_token
+        return result_string, 200
+
+    return resp.text, resp.status_code
+
+app.run(debug=True)
